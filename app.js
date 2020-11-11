51212c61e2d3d2a6071370bffb8211aafea4a6ed
daleinen7 <daleinen@gmail.com>
2020-11-11 12:44:58 -0600

    add new and index routes/controlers/ and views

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var session = require('express-session')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var postsRouter = require('./routes/posts');
 
 var app = express();
 
@@ -40,6 +41,7 @@ app.use(function(req, res, next) {
 });
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/posts', postsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
