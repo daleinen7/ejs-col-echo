e704656e17e5350d0e73d10a44399043c51b74c2
daleinen7 <daleinen@gmail.com>
2020-11-15 20:52:51 -0600

    store media locally

--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
-const postsCtrl = require('../controllers/posts.js')
+const postsCtrl = require('../controllers/posts.js');
+const multer = require('multer');
+const upload = multer({ dest: './public/media'});
 
 router.get('/', postsCtrl.allPosts);
 router.get('/new', isLoggedIn, postsCtrl.new)
-router.post('/', isLoggedIn, postsCtrl.create)
+router.post('/', isLoggedIn, upload.single('media-upload'), postsCtrl.create)
 
 function isLoggedIn(req,res,next) {
     if(req.isAuthenticated()) {
